d0d56c3b6d8c23dce78ff3f1ed5e8051dda32953
Aaron Cymor Nemeno <aaroncymor@gmail.com>
2023-10-15 11:27:12 +0800

    Exceptions and patterns for improting and reference

---
+++ chichiai/exceptions.py
@@ -0,0 +1,35 @@
+class MethodNotImplementedError(Exception):
+    """
+    Raised when a method is not implemented.
+
+    Args:
+        Exception (Exception): MethodNotImplementedError
+    """
+    pass
+
+
+class EnvironmentError(Exception):
+    """
+    Raised when a environment variable is not set.
+
+    Args:
+        Exception (Exception): EnvironmentError
+    """
+    pass
+
+
+class UnsupportedModelError(Exception):
+    """
+    Raised when unsupported model is used.
+
+    Args:
+        model_name (str): The name of unsupported model
+        Exception (Exception): UnsupportedModelError
+    """
+
+    def __init__(self, model_name):
+        self.model = model_name
+        super().__init__(
+            f"Unsupported model: The model '{model_name}' doesn't exist "
+            f"or is not supported yet."
+        )
---
+++ chichiai/roles/patterns.py
@@ -0,0 +1,2 @@
+EXPERT_FINDER_PATTERN = r'Data Analyst|Data Analysis Theorist|Internet Research Specialist'
+ANALYST_FINDER_PATTERN = r'Data Analyst DF|Data Analyst Generic'
