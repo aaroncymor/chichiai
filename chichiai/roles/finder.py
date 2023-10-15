65e2dd5be4f57c7d28283607395dc76b1719cf3b
Aaron Cymor Nemeno <aaroncymor@gmail.com>
2023-10-15 11:26:52 +0800

    patterns in a separate file and importing it for Finder class use

--- chichiai/roles/finder.py
+++ chichiai/roles/finder.py
@@ -1,18 +1,14 @@
 import re
 
 from .base import BaseRoleFinder
+from .patterns import EXPERT_FINDER_PATTERN, ANALYST_FINDER_PATTERN
 
 
 class ExpertFinder(BaseRoleFinder):
     """"""
+    def _extract_role(response: str, pattern: str) -> str:
 
-    def find_role(self):
-        return "find_role"
-
-    def _extract_role(response: str) -> str:
         # Create a pattern to match any of the substrings
-        pattern = r'Data Analyst|Data Analysis Theorist|Internet Research Specialist'
-
         # Use re.search to find the first match in the input string
         match = re.search(pattern, response)
 
@@ -22,18 +18,15 @@ class ExpertFinder(BaseRoleFinder):
         else:
             # If no match is found, return None
             return None
+
+    def find_role(self, response: str) -> str:
+        return self._extract_role(response, EXPERT_FINDER_PATTERN)
 
 
 class AnalystFinder(BaseRoleFinder):
     """"""
-
-    def find_role(self):
-        return "find_role"
-
-    def _extract_role(response: str) -> str:
+    def _extract_role(response: str, pattern: str) -> str:
         # Create a pattern to match any of the substrings
-        pattern = r'Data Analyst DF|Data Analyst Generic'
-
         # Use re.search to find the first match in the input string
         match = re.search(pattern, response)
 
@@ -43,6 +36,9 @@ class AnalystFinder(BaseRoleFinder):
         else:
             # If no match is found, return None
             return None
+
+    def find_role(self, response: str) -> str:
+        return self._extract_role(response, ANALYST_FINDER_PATTERN)
 
 
 if __name__ == "__main__":
